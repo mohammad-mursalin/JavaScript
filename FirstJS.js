ed2bee039af7c65d22de8d7ad7cd0abfcde5af00
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-17 18:03:30 +0600

    Printing something

---
+++ FirstJS.js
@@ -0,0 +1,7 @@
+// for printing something
+alert("Printing by using alert function");
+
+document.write("Printing by using document.write function");
+
+console.log("Printing by using console.log function");
+
