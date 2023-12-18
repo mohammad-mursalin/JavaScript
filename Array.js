97ee709a61935cd1cb6fa87f0bce5b918846618f
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-18 18:29:27 +0600

    java script array

---
+++ Array.js
@@ -0,0 +1,29 @@
+var names = new Array(4);
+
+names[0] = "sokhina";
+names[1] = "jorina";
+names[2] = "rohima";
+names[3] = "akrara";
+
+console.log(names.length);
+
+console.log(names);
+
+names.push("shefaly");
+
+console.log(names[4]);
+
+var country1 = new Array();
+
+country1[0] = "Bangladesh";
+country1[1] = "Pakistan";
+
+var country2 = ["Nepal","Bhutan"];
+
+var country = country1.concat(country2);
+
+console.log(country);
+
+country.pop();
+
+console.log(country);
