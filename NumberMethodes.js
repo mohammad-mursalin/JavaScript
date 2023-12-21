23646a9490a680ccb66bb497cad18aa3608e9396
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-21 22:01:06 +0600

    Number Methodes

---
+++ NumberMethodes.js
@@ -0,0 +1,21 @@
+var num = 123;
+
+var n = toString(num);
+
+console.log(typeof(num));
+
+document.write(num);
+
+console.log(n);
+
+var name = "123";
+
+name = parseInt(name);
+
+document.write(name);
+
+var number = 2.34567;
+
+console.log(number.toFixed(3));  //toFix will show 3 digits after point and round figure
+
+console.log(number.toPrecision(3)); //toPrecision will show total 3 digits
