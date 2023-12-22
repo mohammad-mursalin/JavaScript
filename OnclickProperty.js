28de1cdc0521391d1200fe6391d33ca0251b44c7
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-22 20:45:08 +0600

    code added

--- OnclickProperty.js
+++ OnclickProperty.js
@@ -7,3 +7,13 @@ function para() {
 
     alert("You should'nt")
 }
+
+function button2() {
+
+    document.querySelector("#bp").innerHTML = ("Don't touch me you idiot")
+}
+
+function para2() {
+
+    document.querySelector("#pp").innerHTML = ("You should'nt")
+}
