380bfd23b286a93e5f048d2787e257e871265201
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-22 18:52:13 +0600

    Query selector

---
+++ QuerySelector.js
@@ -0,0 +1,5 @@
+document.querySelector("#id").innerHTML = "id content changed";
+
+document.querySelector(".class").innerHTML = "class content changed";
+
+document.querySelector("h2").innerHTML = "h1 content changed";
