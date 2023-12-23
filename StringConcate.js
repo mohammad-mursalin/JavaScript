0673c4d08042deb579e091da8e195e9f563d562a
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-23 23:23:09 +0600

    String Concate

---
+++ StringConcate.js
@@ -0,0 +1,9 @@
+var firstName = "Mohammad ";
+
+var lastName = "mursalin";
+
+var fullName = firstName + lastName;
+
+document.writeln("My name is " + fullName);
+
+document.write(" Full name is " +firstName.concat(lastName));
