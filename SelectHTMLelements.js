fe8a14ca5f16e43a58e8865423d1d0be5a8573a0
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-23 23:22:23 +0600

    SelectHTMLelements

---
+++ SelectHTMLelements.js
@@ -0,0 +1,8 @@
+document.getElementById("heading1").innerHTML = "Assalamualaikum";
+
+var elements = document.getElementsByTagName("h2");
+
+    elements[0].innerHTML = "everyone";
+    elements[1].innerHTML = "How are you, guys";
+
+document.getElementsByClassName("heading4")[0].innerHTML = "Alhamdulillah";
