64ab3f24adfe2df9e65f9b7c3b27725257ef3799
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-23 20:13:38 +0600

    DOM

---
+++ MoreOnDOM .js
@@ -0,0 +1,9 @@
+var element = document.querySelector("a");
+
+    element.style.textDecoration = "none";
+
+    element.style.color = "green";
+
+    element.href = "www.facebook.com";
+
+    element.style.fontSize = "1.5rem";
