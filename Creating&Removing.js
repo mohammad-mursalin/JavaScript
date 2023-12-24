ae1268923ac051fbf1472fc1054ce450eaf4c963
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-24 18:34:10 +0600

    Creating elements

---
+++ Creating&Removing.js
@@ -0,0 +1,9 @@
+var addTag = document.createElement("h1");
+
+var addText = document.createTextNode("How are you guys");
+
+    addTag.appendChild(addText);
+
+var myDiv = document.getElementById("my-div");
+
+    myDiv.appendChild(addTag);
