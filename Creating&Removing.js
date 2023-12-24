a8e5aaed6045fb0c7f5125721be3927c11d93d1b
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-24 19:04:18 +0600

    adding elements and classes

--- Creating&Removing.js
+++ Creating&Removing.js
@@ -11,3 +11,15 @@ var myDiv = document.getElementById("my-div");
 var removeTag = document.getElementsByTagName("h1")[1];
 
     myDiv.removeChild(removeTag);
+
+var addTag = document.createElement("h1");
+
+var addText = document.createTextNode("Everyone");
+
+    addTag.appendChild(addText);
+
+var Tag = document.getElementsByTagName("h1")[1];
+
+    myDiv.insertBefore(addTag,Tag);
+
+myDiv.classList.add("my-class");
