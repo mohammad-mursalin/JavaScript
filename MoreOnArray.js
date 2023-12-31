94bed48ed8ff1152c2f416eb2bfabaefe8d2dd80
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-31 12:32:00 +0600

    array concatination

--- MoreOnArray.js
+++ MoreOnArray.js
@@ -11,3 +11,21 @@ console.log(myString);
 myArray = myString.split(",");
 
 console.log(myArray);
+
+//concatination
+
+const array1 = [1,2,3,4];
+
+const array2 = [5,6,7,8];
+
+let newArray = array1.concat(array2);
+
+console.log(newArray);
+
+newArray = [...array1,...array2];
+
+console.log(newArray);
+
+newArray = [array1,array2];   //different from the previous one
+
+console.log(newArray);
